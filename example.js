13ad2b9ca69d683f7d20803f67e4d36c976e6a75
Jason <59297610+shawtylikeamelody@users.noreply.github.com>
2021-06-06 12:41:44 -0400

    Update example.js

--- example.js
+++ example.js
@@ -5,10 +5,10 @@ http.createServer((req, res) => proxy({
     url(req){
         return 'https://example.org' + req.url;
     },
-    requestHandler(data){
+    request(data){
         if (data.clientRequest.url == '/test') return data.clientResponse.end('Access denied!');
     },
-    responseHandler(data){
+    response(data){
         data.removeCompression();
         if (data.clientRequest.url == '/cat') data.body = data.body.toString().replace('Example Domain', 'Cats are awesome!');
     },
